af3aba6e45c95cb4c5e69b6814f45b3587e7d15d
ALP300 <aitorpalomino1@gmail.com>
2025-10-17 22:01:27 -0500

    a

--- index.py
+++ index.py
@@ -4,5 +4,9 @@ suma=num+num2
 resta= num-num2
 mult= num * num2
 div= num/num2
+#módulo
+pot= num*2
+mod= num/num2
+print(f"La suma es: {suma}, la resta es: {resta}, la multiplicación: {mult}, la división: {div}")
 
-print(suma)
+print(pot)
